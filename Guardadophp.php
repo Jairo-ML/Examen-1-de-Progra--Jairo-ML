5c64d1abe214fd783d77bed53ee916ab406c9904
Jairo-ML <jairomadrigal0@gmail.com>
2024-07-17 23:40:55 -0600

    Examen

---
+++ Guardadophp.php
@@ -0,0 +1,25 @@
+<?php
+
+function handleReservacion() {
+    $cr_file = 'reservacion.csv';
+
+    try {
+        $reservacion = new reservacion(
+            $_POST['lugar'],
+            $_POST['nombre'],
+            $_POST['apellido'],
+            $_POST['celular'],
+            $_POST['fecha_e'],
+            $_POST['fecha_s']
+        );
+        $reservacion->createFile ($cr_file);
+        echo "Reservación guardada con éxito.";
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    handleReservacion();
+}
+?>
---
+++ examenn.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <title>Reservación de hotel</title>
+</head>
+<body> 
+    <form method="POST" action="Guardadophp.php"> 
+        <label for="lugar">Seleccione el hotel:</label>
+        <br><br>
+        <select name="lugar" required>
+            <option value="" disabled selected>Seleccione un lugar</option>
+            <option value="Hotel Bejuco"> Hotel Bejuco </option>
+            <option value="Hotel Colinas del Viento"> Hotel Colinas del Viento </option>
+            <option value="Hotel Madrigal"> Hotel Madrigal </option>
+            <option value="Hotel York"> Hotel York </option>
+            <option value="Hotel Mañana Soleado">Hotel Mañana Soleado</option>
+        </select>
+        <br><br>
+        <label for="nombre">Digite su nombre:</label>
+        <br><br>
+        <input type="text" placeholder="Escriba su nombre" name="nombre" required>
+        <br><br>
+        <label for="apellido">Digite su apellido:</label>
+        <br><br>
+        <input type="text" placeholder="Escriba su apellido" name="apellido" required>
+        <br><br>
+        <label for="celular">Digite su número de celular:</label>
+        <br><br>
+        <input type="number" placeholder="Escriba su telefono" min="0" name="celular" required>
+        <br><br>
+        <label for="fecha_e">Seleccione la fecha de entrada:</label>
+        <br><br>
+        <input type="date" placeholder="seleccione la fecha" name="fecha_e" required>
+        <br><br>
+        <label for="fecha_s">Seleccione la fecha de salida:</label>
+        <br><br>
+        <input type="date" id="fecha_salida" name="fecha_s" required>
+        <br><br>
+        <input type="submit" name="procesar" value="Agregar" />
+        <br><br>
+    </form>
+    
+    <form method="POST" action="Guardadophp.php">
+        <input type="submit" name="eliminar" value="Eliminar Reservaciones"/>
+    </form>
+</body>
+</html>
